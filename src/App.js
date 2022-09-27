25cbe91d78b9e9cddcc1e4f588cd76f50a21fd03
Tone <tone@kizmet.org>
2022-09-27 11:02:57 -0400

    Cleanup and complete.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
 import React, { useState } from 'react'
-import {
-  TextField,
-  Button,
-  Typography,
-  Tabs,
-  Tab,
-  LinearProgress
-} from '@material-ui/core'
+import { TextField, Button, Typography } from '@material-ui/core'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
 import style from './style'
 import { makeStyles } from '@material-ui/core/styles'
-import { invoice, pay } from './utils'
+
 import GetCertIcon from '@material-ui/icons/GetApp'
 
 import { getCertificates, createCertificate } from '@babbage/sdk'
 
-const useStyles = makeStyles(style, {
-  name: 'CoolCert'
-})
+const useStyles = makeStyles(style, { name: 'CoolCert' })
+
 export default () => {
   const classes = useStyles()
+
   //const [serverURL, setServerURL] = useState('http://localhost:3002')
   //const [serverURL, setServerURL] = useState('https://coolcert.babbage.systems')
   const [serverURL, setServerURL] = useState('https://staging-coolcert.babbage.systems')
-  const [results, setResults] = useState('')
   const [loading, setLoading] = useState(false)
-  const [uploadProgress, setUploadProgress] = useState(0)
+  const [certExists, setCertExists] = useState(false)
+  const [result, setResult] = useState(null)
+
+  const handleReset = async e => {
+    setLoading(false)
+    setCertExists(false)
+    setResult(null)
+  }
 
   const handleGetCert = async e => {
-    console.log("handling get cert")
     e.preventDefault()
     setLoading(true)
     try {
       let certificates = await getCertificates({
         // Specify the types of certificates to request...
-        // Here, we are requesting a "Cool Person Certificate"
+        // Here, we are requesting a "Cool Person Certificate" and the "cool" property of that certificate type.
         types: {'AGfk/WrT1eBDXpz3mcw386Zww2HmqcIn3uY6x4Af1eo=': ['cool']},
         // Provide a list of certifiers you trust. Here, we are trusting
         // CoolCert, the CA that issues Cool Person Certificates.
+        // This is currently and expanded public key hex string. A canonical compressed standard may be adopted in a future version.
+        // The corresponding compressed certifier identifier would be '0247431387e513406817e5e8de00901f8572759012f5ed89b33857295bcc2651f8'
         certifiers: ['0447431387e513406817e5e8de00901f8572759012f5ed89b33857295bcc2651f890b13455f0b59c7b75897033e7ae260834a2397e7c316a0fd21e35e8d81ddd34']
-//        certifiers: ['0247431387e513406817e5e8de00901f8572759012f5ed89b33857295bcc2651f8']
       })
-      //if (typeof certificates === 'object') certificates = certificates.certificates
-      console.log('certificates=', certificates)
       if (certificates.length === 0) {
         // Don't have a certificate yet. Request a new one.
         const certificate = await createCertificate({
@@ -52,9 +50,10 @@ export default () => {
           certifierUrl: serverURL,
           certifierPublicKey: '0247431387e513406817e5e8de00901f8572759012f5ed89b33857295bcc2651f8'
         })
-        console.log(certificate)
+        setResult(certificate)
       } else {
         // We appear to already have one.
+        setCertExists(true)
       }
     } catch (e) {
       console.error(e)
@@ -65,7 +64,6 @@ export default () => {
       }
     } finally {
       setLoading(false)
-      setUploadProgress(0)
     }
   }
 
@@ -78,7 +76,7 @@ export default () => {
         <br />
       </center>
       {true && (
-        <form onSubmit={handleGetCert}>
+        <form>
           <Typography variant='h5'>Server URL</Typography>
           <Typography paragraph>
             Enter the URL of the CoolCert server to interact with
@@ -92,31 +90,45 @@ export default () => {
           />
           <br />
           <br />
-          <br />
           <center className={classes.broadcast_wrap}>
+            {!loading && !result && !certExists && (
             <Button
               onClick={handleGetCert}
               variant='contained'
               color='primary'
               size='large'
-              type='submit'
               disabled={loading}
               startIcon={<GetCertIcon />}
             >
               Get Cool Certificate
             </Button>
+            )}
             <br />
             <br />
             {loading && (
-              <LinearProgress
-                variant={uploadProgress === 0 ? 'indeterminate' : 'determinate'}
-                value={uploadProgress === 0 ? undefined : uploadProgress}
-              />
+              <div>
+                <Typography variant='h4'>Requesting new CoolCert!</Typography>
+              </div>
             )}
-            {results && (
+            {result && (
               <div>
                 <Typography variant='h4'>Success!</Typography>
               </div>
+            )}
+            {certExists && (
+              <div>
+                <Typography variant='h4'>You already have one!</Typography>
+              </div>
+            )}
+            {!loading && (result || certExists) && (
+            <Button
+              onClick={handleReset}
+              variant='contained'
+              color='primary'
+              size='large'
+            >
+              Reset
+            </Button>
             )}
           </center>
         </form>
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export { default as invoice } from './invoice'
-export { default as pay } from './pay'
