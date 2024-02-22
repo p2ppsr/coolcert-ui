6f652105b9c76abb0f5debc7ed87554c9187c982
Ty J Everett <ty@tyweb.us>
2024-02-21 17:16:44 -0800

    No prompt

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import BabbagePrompt from '@babbage/react-prompt'
+// import BabbagePrompt from '@babbage/react-prompt'
 
 ReactDOM.render(
-  <BabbagePrompt
-    customPrompt
-    appName='CoolCert UI'
-    author='Project Babbage'
-    authorUrl='https://projectbabbage.com'
-    description='Get your coolness certified today!'
-    appIcon='/favicon.ico'
-    supportedMetaNet='testnet'
-  >
-    <App />
-  </BabbagePrompt>,
+  // <BabbagePrompt
+  //   customPrompt
+  //   appName='CoolCert UI'
+  //   author='Project Babbage'
+  //   authorUrl='https://projectbabbage.com'
+  //   description='Get your coolness certified today!'
+  //   appIcon='/favicon.ico'
+  //   supportedMetaNet='testnet'
+  // >
+  <App />,
+  // </BabbagePrompt>,
   document.getElementById('root')
 )
